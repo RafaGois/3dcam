cf71b51fe491354e2cb9affb10e126b1d9d3c213
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2023-11-12 00:09:06 -0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,32 @@ import { Canvas, useFrame } from "@react-three/fiber"
 import Experience from './components/Experience'
 import { useRef, useState } from 'react'
 import { AccumulativeShadows, Environment, Lightformer, PerformanceMonitor, RandomizedLight } from '@react-three/drei';
+import Overlay from './Overlay'
 
 function App() {
   const [perfSucks, degrade] = useState(false);
 
   return (
-    <Canvas
-      shadows
-      dpr={[1, perfSucks ? 1.5 : 2]}
-      eventSource={document.getElementById('root')}
-      eventPrefix="client"
-      camera={{ position: [20, 0.9, 20], fov: 20 }}
-    >
-      <PerformanceMonitor onDecline={() => degrade(true)} />
-      <color attach="background" args={['#f0f0f0']} />
-      <group position={[0, -0.5, 0]} rotation={[0, -0.75, 0]}>
-        <Experience />
-        <AccumulativeShadows frames={100} alphaTest={0.85} opacity={0.8} color="red" scale={20} position={[0, -0.005, 0]}>
-          <RandomizedLight amount={8} radius={6} ambient={0.5} intensity={1} position={[-1.5, 2.5, -2.5]} bias={0.001} />
-        </AccumulativeShadows>
-      </group>
-      <Env perfSucks={perfSucks} />
-    </Canvas>
+    <div style={{height: "100vh", position: "relative"}}>
+      <Canvas
+        shadows
+        dpr={[1, perfSucks ? 1.5 : 2]}
+        eventSource={document.getElementById('root')}
+        eventPrefix="client"
+        camera={{ position: [20, 0.9, 20], fov: 20 }}
+      >
+        <PerformanceMonitor onDecline={() => degrade(true)} />
+
+        <group position={[0, 0, 0]} rotation={[0, 0, 0]}>
+          <Experience />
+          <AccumulativeShadows frames={100} alphaTest={0.85} opacity={0.8} color="red" scale={20} position={[0, -0.005, 0]}>
+            <RandomizedLight amount={8} radius={6} ambient={0.5} intensity={1} position={[-1.5, 2.5, -2.5]} bias={0.001} />
+          </AccumulativeShadows>
+        </group>
+        <Env perfSucks={perfSucks} />
+      </Canvas>
+      <Overlay />
+    </div>
   )
 }
 
--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -1,24 +1,7 @@
 export default function Overlay() {
     return (
-      <>
-        <div className="char" style={{ top: 40, left: 40 }}>
-          P
-        </div>
-        <div className="char" style={{ top: 40, left: '20vw' }}>
-          M
-        </div>
-        <div className="char" style={{ top: 40, left: '40vw' }}>
-          N
-        </div>
-        <div className="char" style={{ top: '20vw', left: '20vw' }}>
-          D
-        </div>
-        <div className="char" style={{ bottom: 40, left: '40vw' }}>
-          R
-        </div>
-        <div className="char" style={{ bottom: 40, left: '60vw' }}>
-          S
-        </div>
+      <div style={{position: "absolute", top: 0, width: "100vw", height: "100vh"}}>
+        <h1>HIREBOR</h1>
         <div style={{ position: 'absolute', top: 40, right: 160, fontSize: '15px', textAlign: 'right' }}>
           A DEV
           <br />
@@ -27,7 +10,7 @@ export default function Overlay() {
         <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '15px', textAlign: 'right', fontVariantNumeric: 'tabular-nums' }}>
           —
           <br />
-          08/01/23
+          - 2023
         </div>
         <svg style={{ position: 'absolute', right: 40, top: '50%' }} width="54" height="23" viewBox="0 0 54 23" fill="none" xmlns="http://www.w3.org/2000/svg">
           <line y1="1.5" x2="54" y2="1.5" stroke="black" strokeWidth="3" />
@@ -48,7 +31,7 @@ export default function Overlay() {
           </div>
           <br />
         </div>
-      </>
+      </div>
     )
   }
   
